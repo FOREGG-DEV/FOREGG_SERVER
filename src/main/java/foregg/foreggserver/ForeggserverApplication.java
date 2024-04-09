e358d050d60a7552b5771a27d975dfadc58588f5
kimdongjun <r2222911r@ajou.ac.kr>
2024-04-09 13:45:19 +0900

    initializing

---
+++ src/main/java/foregg/foreggserver/ForeggserverApplication.java
@@ -0,0 +1,13 @@
+package foregg.foreggserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeggserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeggserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/foregg/foreggserver/ForeggserverApplicationTests.java
@@ -0,0 +1,13 @@
+package foregg.foreggserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeggserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
