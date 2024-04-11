d5ea92e53e0f91f0ac9fe6d87bfcbfcade251a7f
kimdongjun <r2222911r@ajou.ac.kr>
2024-04-12 01:38:28 +0900

    :sparkles: feat: 알림 Entity 추가
    Notification Entity 추가

---
+++ src/main/java/foregg/foreggserver/domain/Notification.java
@@ -0,0 +1,31 @@
+package foregg.foreggserver.domain;
+
+import foregg.foreggserver.domain.common.BaseEntity;
+import foregg.foreggserver.domain.enums.NotifType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Notification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private NotifType notificationType;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
