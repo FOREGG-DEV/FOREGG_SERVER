b13b1c0f3d0d5316acdf98c5c7677b20e3dbd23d
kimdongjun <r2222911r@ajou.ac.kr>
2024-04-12 12:56:29 +0900

    :sparkles: feat: 부작용 Entity 추가
    SideEffect Entity 추가

---
+++ src/main/java/foregg/foreggserver/domain/SideEffect.java
@@ -0,0 +1,27 @@
+package foregg.foreggserver.domain;
+
+import foregg.foreggserver.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SideEffect extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String image;
+
+}
