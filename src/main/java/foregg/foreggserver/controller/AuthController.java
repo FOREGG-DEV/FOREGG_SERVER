4b19064a61d1661bcce258156666d3feff67542f
kimdongjun <r2222911r@ajou.ac.kr>
2024-04-16 01:09:59 +0900

    :sparkles: feat: 회원가입/로그인 API 추가
    회원가입/로그인 API 추가

---
+++ src/main/java/foregg/foreggserver/controller/AuthController.java
@@ -0,0 +1,59 @@
+package foregg.foreggserver.controller;
+
+import foregg.foreggserver.apiPayload.ApiResponse;
+import foregg.foreggserver.apiPayload.code.status.SuccessStatus;
+import foregg.foreggserver.dto.UserJoinRequestDTO;
+import foregg.foreggserver.jwt.JwtTokenProvider;
+import foregg.foreggserver.service.UserQueryService;
+import foregg.foreggserver.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+@Tag(name = "로그인/회원가입 API")
+public class AuthController {
+
+    private final UserService userService;
+    private final UserQueryService kakaoQueryService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+
+    // 카카오 로그인을 위해 회원가입 여부 확인, 이미 회원이면 Jwt 토큰 발급
+    @Operation(summary = "카카오 로그인 API")
+    @Parameters({
+            @Parameter(name = "accessToken", description = "헤더의 엑세스 토큰입니다.")
+    })
+    @PostMapping("/login")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER4002", description = "존재하지 않는 사용자입니다."),
+    })
+    public ApiResponse<SuccessStatus> userCheck(@RequestHeader String accessToken) {
+        kakaoQueryService.isSignedUp(accessToken);
+        return ApiResponse.onSuccess(SuccessStatus._OK);
+    }
+
+
+    //회원가입이 완료되었을때 JWT를 반환하게끔
+    @Operation(summary = "회원가입 API")
+    @Parameters({
+            @Parameter(name = "accessToken", description = "헤더의 엑세스 토큰입니다."),
+    })
+    @PostMapping("/join")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+    })
+    public ApiResponse<String> join(@RequestHeader String accessToken, @RequestBody UserJoinRequestDTO request) {
+        String idAndToken = userService.join(accessToken, request);
+        return ApiResponse.onSuccess(idAndToken);
+    }
+}
