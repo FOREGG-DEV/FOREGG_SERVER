69bf082ce9e85b1ed24a76d967325cc3775fc59b
kimdongjun <r2222911r@ajou.ac.kr>
2024-04-16 01:33:07 +0900

    :sparkles: feat: CustomUserDetailService 추가
    CustomUserDetailService 추가

---
+++ src/main/java/foregg/foreggserver/jwt/CustomUserDetailService.java
@@ -0,0 +1,24 @@
+package foregg.foreggserver.jwt;
+
+import foregg.foreggserver.apiPayload.code.status.ErrorStatus;
+import foregg.foreggserver.apiPayload.exception.handler.UserHandler;
+import foregg.foreggserver.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String keyCode) throws UsernameNotFoundException {
+        return userRepository.findByKeyCode(keyCode)
+                .orElseThrow(() -> new UserHandler(ErrorStatus.USER_NOT_FOUND));
+    }
+
+}
