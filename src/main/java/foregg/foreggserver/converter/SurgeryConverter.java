eac2f1132d264b841e3b2cf8ec97c27d2eba8190
kimdongjun <r2222911r@ajou.ac.kr>
2024-04-16 21:29:52 +0900

    :zap: fix: Surgery, User 연관관계 매핑 수정에 따른 인자 변경
    인자 User 삭제

--- src/main/java/foregg/foreggserver/converter/SurgeryConverter.java
+++ src/main/java/foregg/foreggserver/converter/SurgeryConverter.java
@@ -2,15 +2,15 @@ package foregg.foreggserver.converter;
 
 import foregg.foreggserver.domain.Surgery;
 import foregg.foreggserver.domain.User;
+import foregg.foreggserver.domain.enums.SurgeryType;
 import foregg.foreggserver.dto.UserJoinRequestDTO;
 
 public class SurgeryConverter {
 
-    public static Surgery toSurgery(UserJoinRequestDTO surgeryInfo, User user) {
+    public static Surgery toSurgery(UserJoinRequestDTO surgeryInfo) {
         return Surgery.builder()
                 .surgeryType(surgeryInfo.getSurgeryType())
                 .startAt(surgeryInfo.getStartAt())
-                .count(surgeryInfo.getCount())
-                .user(user).build();
+                .count(surgeryInfo.getCount()).build();
     }
 }
