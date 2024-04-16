7855ddc97d1b5171618c0d2f470342a937fc44eb
kimdongjun <r2222911r@ajou.ac.kr>
2024-04-16 21:30:41 +0900

    :zap: fix: DTO 수정
    NoArgs, AllArgs 추가

--- src/main/java/foregg/foreggserver/dto/UserJoinRequestDTO.java
+++ src/main/java/foregg/foreggserver/dto/UserJoinRequestDTO.java
@@ -2,14 +2,18 @@ package foregg.foreggserver.dto;
 
 import foregg.foreggserver.domain.Surgery;
 import foregg.foreggserver.domain.enums.SurgeryType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.Date;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserJoinRequestDTO {
 
     private SurgeryType surgeryType;
