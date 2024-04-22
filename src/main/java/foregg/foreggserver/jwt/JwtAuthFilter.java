a79f8452f70cafaf7633ead919f5e3eb688478b8
kimdongjun <r2222911r@ajou.ac.kr>
2024-04-22 20:34:10 +0900

    :recycle: chore: 어노테이션 추가
    어노테이션 추가

--- src/main/java/foregg/foreggserver/jwt/JwtAuthFilter.java
+++ src/main/java/foregg/foreggserver/jwt/JwtAuthFilter.java
@@ -7,13 +7,17 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Component
+@Configuration
 public class JwtAuthFilter extends GenericFilterBean {
 
     private final JwtTokenProvider jwtTokenProvider;
