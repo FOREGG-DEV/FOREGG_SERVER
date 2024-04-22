62af13d6f53343024158320f7ea7dccb2f09cb64
kimdongjun <r2222911r@ajou.ac.kr>
2024-04-22 20:08:43 +0900

    :recycle: chore: getCurrentUser 로직 변경
    getCurrentUser 로직 변경

--- src/main/java/foregg/foreggserver/jwt/SecurityUtil.java
+++ src/main/java/foregg/foreggserver/jwt/SecurityUtil.java
@@ -1,12 +1,19 @@
 package foregg.foreggserver.jwt;
 
+import foregg.foreggserver.domain.User;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class SecurityUtil {
-    public static String getCurrentUserName(){
-        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        return user.getUsername();
+    public static String getCurrentUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetails userDetails = (UserDetails) principal;
+        String username = userDetails.getUsername();
+
+        return username;
     }
+
 }
