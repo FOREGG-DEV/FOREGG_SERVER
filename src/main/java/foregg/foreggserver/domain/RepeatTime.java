c9543ed4211e475581d60c6acc90086e06dbf0bc
kimdongjun <r2222911r@ajou.ac.kr>
2024-04-30 15:22:08 +0900

    :sparkles: feat: RepeatTime 클래스 생성
    RepeatTime 클래스 생성

---
+++ src/main/java/foregg/foreggserver/domain/RepeatTime.java
@@ -0,0 +1,29 @@
+package foregg.foreggserver.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import foregg.foreggserver.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RepeatTime extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String time;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "record_id")
+    private Record record;
+
+}
