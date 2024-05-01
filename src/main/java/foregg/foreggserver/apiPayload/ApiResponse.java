da820b532f35058fb4353c97c806597cbbb4c400
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-01 23:46:01 +0900

    :recycle: recycle: ApiResponse 메서드 추가
    ApiResponse 유저 로그인 실패 관련 메서드 추가

--- src/main/java/foregg/foreggserver/apiPayload/ApiResponse.java
+++ src/main/java/foregg/foreggserver/apiPayload/ApiResponse.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import foregg.foreggserver.apiPayload.code.BaseCode;
+import foregg.foreggserver.apiPayload.code.status.ErrorStatus;
 import foregg.foreggserver.apiPayload.code.status.SuccessStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -34,6 +35,10 @@ public class ApiResponse<T> {
     // 실패한 경우 응답 생성
     public static <T> ApiResponse<T> onFailure(String code, String message, T data){
         return new ApiResponse<>(false, code, message, data);
+    }
+
+    public static <T> ApiResponse<T> onFailureOnLogin(T data){
+        return new ApiResponse<>(false, ErrorStatus.USER_NEED_JOIN.getCode(), ErrorStatus.USER_NEED_JOIN.getMessage(), data);
     }
 
     public static <T> ApiResponse<T> onSuccess(){
