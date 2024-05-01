6959b9dd9e409d0801c47ffa0caab025d5c5934a
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-01 23:49:42 +0900

    :recycle: recycle: 시큐리티 유틸 예외처리
    시큐리티 유틸 예외처리

--- src/main/java/foregg/foreggserver/jwt/SecurityUtil.java
+++ src/main/java/foregg/foreggserver/jwt/SecurityUtil.java
@@ -1,5 +1,7 @@
 package foregg.foreggserver.jwt;
 
+import foregg.foreggserver.apiPayload.code.status.ErrorStatus;
+import foregg.foreggserver.apiPayload.exception.handler.UserHandler;
 import foregg.foreggserver.domain.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,11 +11,17 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class SecurityUtil {
 
     public static String getCurrentUser() {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        UserDetails userDetails = (UserDetails) principal;
-        String username = userDetails.getUsername();
 
-        return username;
+        try {
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            UserDetails userDetails = (UserDetails) principal;
+            String username = userDetails.getUsername();
+
+            return username;
+        } catch (Exception e) {
+            throw new UserHandler(ErrorStatus.USER_NOT_FOUND);
+        }
+
     }
 
 }
