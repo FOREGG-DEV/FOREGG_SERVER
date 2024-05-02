87a41b06466b0d36a6a3283c613f2a7c9c438964
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-03 01:00:16 +0900

    :sparkles: feat: ChallengeResponseDTO 생성
    ChallengeResponseDTO 생성

---
+++ src/main/java/foregg/foreggserver/dto/challengeDTO/ChallengeResponseDTO.java
@@ -0,0 +1,22 @@
+package foregg.foreggserver.dto.challengeDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChallengeResponseDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private int participants;
+    private List<String> successDays;
+
+}
