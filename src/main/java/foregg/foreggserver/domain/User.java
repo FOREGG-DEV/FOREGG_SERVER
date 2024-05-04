6586b872075eedbbc0c6174026c08ba23df3e697
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-04 14:57:01 +0900

    :sparkles: feat: User 엔티티 List<Ledger> 필드 추가
    User 엔티티 List<Ledger> 필드 추가

--- src/main/java/foregg/foreggserver/domain/User.java
+++ src/main/java/foregg/foreggserver/domain/User.java
@@ -58,6 +58,9 @@ public class User extends BaseEntity implements UserDetails {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<ChallengeParticipation> challengeParticipations;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Ledger> ledgers;
+
     // Jwt 전용 설정 (UserDetails 인터페이스 구현)
 
     @ElementCollection(fetch = FetchType.EAGER) //roles 컬렉션
