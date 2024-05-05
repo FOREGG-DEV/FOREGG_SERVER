98f3f0270dffd487486bc866205d09919994af6e
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-06 00:54:26 +0900

    :sparkles: feat: MyPageResponseDTO 생성
    MyPageResponseDTO 생성

---
+++ src/main/java/foregg/foreggserver/dto/myPageDTO/MyPageResponseDTO.java
@@ -0,0 +1,21 @@
+package foregg.foreggserver.dto.myPageDTO;
+
+import foregg.foreggserver.domain.enums.SurgeryType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyPageResponseDTO {
+
+    private String nickname;
+    private SurgeryType surgeryType;
+    private int count;
+    private String startDate;
+    private String spouse;
+
+}
