1f6c0ca785a114d86b7ddf104ce8aa5fc9ce8bf9
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-06 16:11:36 +0900

    :recycle: refactor: 홈 응답 수정
    홈 응답 수정에 따른 Converter 변경

--- src/main/java/foregg/foreggserver/converter/HomeConverter.java
+++ src/main/java/foregg/foreggserver/converter/HomeConverter.java
@@ -1,17 +1,27 @@
 package foregg.foreggserver.converter;
 
 import foregg.foreggserver.domain.Record;
+import foregg.foreggserver.domain.RepeatTime;
 import foregg.foreggserver.dto.homeDTO.HomeRecordResponseDTO;
 import foregg.foreggserver.dto.homeDTO.HomeResponseDTO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomeConverter {
 
     public static HomeRecordResponseDTO toHomeRecordResponseDTO(Record record) {
+
+        List<String> resultList = new ArrayList<>();
+        List<RepeatTime> repeatTimes = record.getRepeatTimes();
+        for (RepeatTime rt : repeatTimes) {
+            resultList.add(rt.getTime());
+        }
+
         return HomeRecordResponseDTO.builder()
                 .id(record.getId())
                 .recordType(record.getType())
+                .times(resultList)
                 .name(record.getName())
                 .memo(record.getMemo()).build();
     }
@@ -22,4 +32,5 @@ public class HomeConverter {
                 .todayDate(todayDate)
                 .homeRecordResponseDTO(homeRecordResponseDTOS).build();
     }
+
 }
