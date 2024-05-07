2d04839213e7265d97cd5849f7556230a9772bea
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-07 13:46:01 +0900

    :sparkles: feat: UserService 메서드 추가
    UserService husbandJoin 메서드 추가

--- src/main/java/foregg/foreggserver/service/userService/UserService.java
+++ src/main/java/foregg/foreggserver/service/userService/UserService.java
@@ -5,30 +5,24 @@ import foregg.foreggserver.converter.SurgeryConverter;
 import foregg.foreggserver.converter.UserConverter;
 import foregg.foreggserver.domain.Surgery;
 import foregg.foreggserver.domain.User;
+import foregg.foreggserver.dto.userDTO.UserHusbandJoinRequestDTO;
 import foregg.foreggserver.dto.userDTO.UserJoinRequestDTO;
 import foregg.foreggserver.dto.kakaoDTO.KakaoUserInfoResponse;
 import foregg.foreggserver.dto.userDTO.UserResponseDTO;
 import foregg.foreggserver.jwt.JwtTokenProvider;
-import foregg.foreggserver.jwt.SecurityUtil;
 import foregg.foreggserver.repository.SurgeryRepository;
 import foregg.foreggserver.repository.UserRepository;
-import foregg.foreggserver.service.userService.KakaoRequestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
-import static foregg.foreggserver.apiPayload.code.status.ErrorStatus.USER_NEED_JOIN;
+import static foregg.foreggserver.apiPayload.code.status.ErrorStatus.INVALID_SPOUSE_CODE;
 
 @Service
 @Slf4j
@@ -68,6 +62,25 @@ public class UserService {
         userRepository.save(UserConverter.toUser(userInfo, keyCode, surgery, dto));
 
         return UserConverter.toUserResponseDTO(keyCode, jwt, null);
+    }
+
+    public UserResponseDTO husbandJoin(String token, UserHusbandJoinRequestDTO dto) {
+        // 배우자 코드가 존재하지 않거나, 해당 배우자 코드를 가지고 있는 유저가 이미 남편을 등록해놓은 경우 예외처리
+        User wife = userRepository.findBySpouseCode(dto.getSpouseCode()).orElseThrow(() -> new UserHandler(INVALID_SPOUSE_CODE));
+        if (wife.getSpouse() != null) {
+            throw new UserHandler(INVALID_SPOUSE_CODE);
+        }
+        KakaoUserInfoResponse userInfo = kakaoRequestService.getUserInfo(token);
+        Long userId = userInfo.getId();
+        String jwt = jwtTokenProvider.createToken(userId.toString());
+        String keyCode = jwtTokenProvider.getUserPk(jwt);
+        User husband = UserConverter.toHusband(userInfo, keyCode, wife, dto);
+
+        wife.setSpouse(husband);
+        userRepository.save(husband);
+
+        return UserConverter.toUserResponseDTO(keyCode, jwt, null);
+
     }
 
 }
