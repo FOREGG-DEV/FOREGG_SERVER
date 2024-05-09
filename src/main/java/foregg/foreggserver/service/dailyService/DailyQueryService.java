15397458211a86d17db86fd230cb6e060726d2ec
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-09 18:22:46 +0900

    :sparkles: feat: DailyQueryService 생성
    DailyQueryService 생성

---
+++ src/main/java/foregg/foreggserver/service/dailyService/DailyQueryService.java
@@ -0,0 +1,43 @@
+package foregg.foreggserver.service.dailyService;
+
+import foregg.foreggserver.converter.DailyConverter;
+import foregg.foreggserver.domain.Daily;
+import foregg.foreggserver.domain.User;
+import foregg.foreggserver.dto.dailyDTO.DailyResponseDTO;
+import foregg.foreggserver.dto.dailyDTO.DailyTotalResponseDTO;
+import foregg.foreggserver.jwt.SecurityUtil;
+import foregg.foreggserver.repository.DailyRepository;
+import foregg.foreggserver.service.userService.UserQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class DailyQueryService {
+
+    private final DailyRepository dailyRepository;
+    private final UserQueryService userQueryService;
+
+    public DailyTotalResponseDTO getDaily() {
+        List<DailyResponseDTO> resultList = new ArrayList<>();
+        User user = userQueryService.getUser(SecurityUtil.getCurrentUser());
+        Optional<List<Daily>> daily = dailyRepository.findByUser(user);
+        if (daily.isEmpty()) {
+            return null;
+        }
+        List<Daily> dailyList = daily.get();
+        for (Daily d : dailyList) {
+            resultList.add(DailyConverter.toDailyResponseDTO(d));
+        }
+        return DailyTotalResponseDTO.builder()
+                .dailyResponseDTO(resultList)
+                .build();
+    }
+
+}
