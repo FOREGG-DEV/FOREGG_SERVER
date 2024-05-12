07eda938ad9a7d592fd81414fdd4b40810c89f74
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-12 12:49:25 +0900

    :lock: security: 인가 설정
    인가 설정

--- src/main/java/foregg/foreggserver/controller/ChallengeController.java
+++ src/main/java/foregg/foreggserver/controller/ChallengeController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/challenge")
 @Tag(name = "챌린지 API")
+@PreAuthorize("hasRole('ROLE_WIFE')")
 public class ChallengeController {
 
     private final ChallengeQueryService challengeQueryService;
