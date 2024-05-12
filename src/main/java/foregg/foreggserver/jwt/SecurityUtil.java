7b4288e3812d1f578643d45ad361a5bc444935d8
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-12 12:52:54 +0900

    :sparkles: feat: 현재 유저 여자인지 남자인지
    현재 유저 판단 로직 추가

--- src/main/java/foregg/foreggserver/jwt/SecurityUtil.java
+++ src/main/java/foregg/foreggserver/jwt/SecurityUtil.java
@@ -2,16 +2,15 @@ package foregg.foreggserver.jwt;
 
 import foregg.foreggserver.apiPayload.code.status.ErrorStatus;
 import foregg.foreggserver.apiPayload.exception.handler.UserHandler;
-import foregg.foreggserver.domain.User;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 public class SecurityUtil {
 
     public static String getCurrentUser() {
-
         try {
             Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
             UserDetails userDetails = (UserDetails) principal;
@@ -21,7 +20,18 @@ public class SecurityUtil {
         } catch (Exception e) {
             throw new UserHandler(ErrorStatus.USER_NOT_FOUND);
         }
-
     }
 
+    public static boolean ifCurrentUserIsHusband() {
+        try {
+            Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+            if (authorities.contains("ROLE_HUSBAND")) {
+                return true;
+            }else{
+                return false;
+            }
+        } catch (Exception e) {
+            throw new UserHandler(ErrorStatus.USER_NOT_FOUND);
+        }
+    }
 }
