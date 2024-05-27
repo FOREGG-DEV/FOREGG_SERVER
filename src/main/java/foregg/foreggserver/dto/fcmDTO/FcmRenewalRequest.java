30c7b5e01f0bfde35c9e120be098d53217474948
kimdongjun <r2222911r@ajou.ac.kr>
2024-05-27 23:35:03 +0900

    :sparkles: refactor: FcmRenewalRequest 생성
    FcmRenewalRequest 생성

---
+++ src/main/java/foregg/foreggserver/dto/fcmDTO/FcmRenewalRequest.java
@@ -0,0 +1,16 @@
+package foregg.foreggserver.dto.fcmDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FcmRenewalRequest{
+
+    private String fcm;
+
+}
