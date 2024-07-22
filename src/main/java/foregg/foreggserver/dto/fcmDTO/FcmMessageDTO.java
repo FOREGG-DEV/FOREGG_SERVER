5c8ea713cf6b58bf5f7f6409481d3c6e0f46b455
kimdongjun <r2222911r@ajou.ac.kr>
2024-07-22 13:33:15 +0900

    :recycle: refactor: fcm 우선순위 적용
    fcm 우선순위 적용

--- src/main/java/foregg/foreggserver/dto/fcmDTO/FcmMessageDTO.java
+++ src/main/java/foregg/foreggserver/dto/fcmDTO/FcmMessageDTO.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-//FCM에 전송하는 DTO
+// FCM에 전송하는 DTO
 @Getter
 @Builder
 public class FcmMessageDTO {
@@ -15,8 +15,10 @@ public class FcmMessageDTO {
     @AllArgsConstructor
     @Getter
     public static class Message {
-        private FcmMessageDTO.Notification data;
+        private FcmMessageDTO.Notification notification;
+        private FcmMessageDTO.Data data;
         private String token;
+        private FcmMessageDTO.Android android;
     }
 
     @Builder
@@ -25,8 +27,21 @@ public class FcmMessageDTO {
     public static class Notification {
         private String title;
         private String body;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Data {
         private String type;
         private String targetId;
         private String time;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Android {
+        private String priority;
     }
 }
