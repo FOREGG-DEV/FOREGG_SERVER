a3f1e7aec0585dc53371d116908858f7a29d8057
kimdongjun <r2222911r@ajou.ac.kr>
2024-09-03 14:41:51 +0900

    :recycle: refactor: toMyPageRequestDTO 메서드 추가
    toMyPageRequestDTO 메서드 추가

--- src/main/java/foregg/foreggserver/converter/SurgeryConverter.java
+++ src/main/java/foregg/foreggserver/converter/SurgeryConverter.java
@@ -1,6 +1,7 @@
 package foregg.foreggserver.converter;
 
 import foregg.foreggserver.domain.Surgery;
+import foregg.foreggserver.dto.myPageDTO.MyPageRequestDTO;
 import foregg.foreggserver.dto.userDTO.UserJoinRequestDTO;
 
 public class SurgeryConverter {
@@ -10,5 +11,13 @@ public class SurgeryConverter {
                 .surgeryType(surgeryInfo.getSurgeryType())
                 .startAt(surgeryInfo.getStartAt())
                 .count(surgeryInfo.getCount()).build();
+    }
+
+    public static MyPageRequestDTO toMyPageRequestDTO(Surgery surgery) {
+        return MyPageRequestDTO.builder()
+                .surgeryType(surgery.getSurgeryType())
+                .count(surgery.getCount())
+                .startDate(surgery.getStartAt())
+                .build();
     }
 }
