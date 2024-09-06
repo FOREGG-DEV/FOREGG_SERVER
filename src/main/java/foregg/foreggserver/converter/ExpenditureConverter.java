e5aeba7fa72f3f22c675dd77ef6440a9f7b8433a
kimdongjun <r2222911r@ajou.ac.kr>
2024-09-06 16:01:05 +0900

    :sparkles: feat: ExpenditureConverter 클래스 생성
    ExpenditureConverter 클래스 생성

---
+++ src/main/java/foregg/foreggserver/converter/ExpenditureConverter.java
@@ -0,0 +1,27 @@
+package foregg.foreggserver.converter;
+
+import foregg.foreggserver.domain.Expenditure;
+import foregg.foreggserver.domain.Ledger;
+import foregg.foreggserver.dto.expenditureDTO.ExpenditureRequestDTO;
+import foregg.foreggserver.dto.ledgerDTO.LedgerRequestDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpenditureConverter {
+
+    public static List<Expenditure> toExpenditure(LedgerRequestDTO dto, Ledger ledger) {
+        List<ExpenditureRequestDTO> expenditureRequestDTOList = dto.getExpenditureRequestDTOList();
+        List<Expenditure> expenditures = new ArrayList<>();
+
+        for (ExpenditureRequestDTO eqd : expenditureRequestDTOList) {
+            Expenditure expenditure = Expenditure.builder()
+                    .name(eqd.getName())
+                    .amount(eqd.getAmount())
+                    .ledger(ledger)
+                    .build();
+            expenditures.add(expenditure);
+        }
+        return expenditures;
+    }
+}
