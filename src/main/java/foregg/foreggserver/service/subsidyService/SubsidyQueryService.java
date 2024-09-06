00eb132e9d1d8ae688823154103ee7a33bcd22b9
kimdongjun <r2222911r@ajou.ac.kr>
2024-09-06 15:59:31 +0900

    :recycle: refactor: SubsidyQueryService 로직 수정
    SubsidyQueryService 로직 수정

--- src/main/java/foregg/foreggserver/service/subsidyService/SubsidyQueryService.java
+++ src/main/java/foregg/foreggserver/service/subsidyService/SubsidyQueryService.java
@@ -2,23 +2,31 @@ package foregg.foreggserver.service.subsidyService;
 
 import foregg.foreggserver.apiPayload.exception.handler.SubsidyHandler;
 import foregg.foreggserver.converter.SubsidyConverter;
+import foregg.foreggserver.domain.Expenditure;
+import foregg.foreggserver.domain.Ledger;
 import foregg.foreggserver.domain.Subsidy;
 import foregg.foreggserver.domain.User;
+import foregg.foreggserver.dto.ledgerDTO.LedgerResponseDTO;
 import foregg.foreggserver.dto.subsidyDTO.SubsidyResponseDTO;
 import foregg.foreggserver.jwt.SecurityUtil;
 import foregg.foreggserver.repository.SubsidyRepository;
 import foregg.foreggserver.service.userService.UserQueryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static foregg.foreggserver.apiPayload.code.status.ErrorStatus.NOT_FOUND_MY_SUBSIDY;
+import static foregg.foreggserver.apiPayload.code.status.ErrorStatus.SUBSIDY_ALREADY_EXIST;
 
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
+@Slf4j
 public class SubsidyQueryService {
 
     private final SubsidyRepository subsidyRepository;
@@ -33,6 +41,45 @@ public class SubsidyQueryService {
         User user = userQueryService.getUser(SecurityUtil.getCurrentUser());
         List<Subsidy> subsidies = subsidyRepository.findByUserAndCount(user, count);
         // 회차 날짜 로직을 구해서 null 값을 대체해야함
-        return SubsidyConverter.toDetailResponseDTO(null, subsidies);
+        return SubsidyConverter.toDetailResponseDTO(subsidies);
     }
+
+    public Subsidy getSubsidyByUserCountName(int count, String name) {
+        User user = userQueryService.getUser(SecurityUtil.getCurrentUser());
+        return subsidyRepository.findByUserAndCountAndNickname(user, count, name);
+    }
+
+    public List<Subsidy> getSubsidyByUserAndCount(int count) {
+        User user = userQueryService.getUser(SecurityUtil.getCurrentUser());
+        List<Subsidy> subsidies = subsidyRepository.findByUserAndCount(user, count);
+        return subsidies;
+    }
+
+    public List<LedgerResponseDTO.SubsidyAvailable> toSubsidyAvailable(List<Ledger> ledgers, int count) {
+        User user = userQueryService.getUser(SecurityUtil.getCurrentUser());
+        List<LedgerResponseDTO.SubsidyAvailable> result = new ArrayList<>();
+        List<String> subsidies = ledgers.stream()
+                .flatMap(ledger -> ledger.getExpenditureList().stream()) // 각 Ledger의 Expenditure를 스트림으로 평탄화
+                .map(Expenditure::getName) // Expenditure의 name 추출
+                .distinct() // 중복 제거
+                .collect(Collectors.toList());
+
+        subsidies.remove("개인");
+        for (String subsidy : subsidies) {
+            Subsidy sub = subsidyRepository.findByUserAndCountAndNickname(user, count, subsidy);
+            LedgerResponseDTO.SubsidyAvailable dto = LedgerResponseDTO.SubsidyAvailable.builder().nickname(subsidy).amount(sub.getAvailable()).build();
+            result.add(dto);
+        }
+        return result;
+    }
+
+    public void subsidyExist(String nickname, int count) {
+        User user = userQueryService.getUser(SecurityUtil.getCurrentUser());
+        Subsidy subsidy = subsidyRepository.findByUserAndCountAndNickname(user, count, nickname);
+        if (subsidy != null) {
+            throw new SubsidyHandler(SUBSIDY_ALREADY_EXIST);
+        }
+
+    }
+
 }
