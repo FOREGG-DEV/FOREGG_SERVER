9b2eb5286ba85219f7ddd31332b29e54115580ca
kimdongjun <r2222911r@ajou.ac.kr>
2024-09-06 16:01:48 +0900

    :sparkles: feat: Ledger 클래스 생성
    Ledger 클래스 생성

---
+++ src/main/java/foregg/foreggserver/domain/Ledger.java
@@ -0,0 +1,41 @@
+package foregg.foreggserver.domain;
+
+import foregg.foreggserver.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Ledger extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String date;
+
+    @Column(nullable = false)
+    private int count;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String memo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @OneToMany(mappedBy = "ledger")
+    private List<Expenditure> expenditureList = new ArrayList<>();
+}
