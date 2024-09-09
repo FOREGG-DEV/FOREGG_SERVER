b70d18ac6f6097580119f7c207b61e2c84ff8c2b
kimdongjun <r2222911r@ajou.ac.kr>
2024-09-09 13:37:51 +0900

    :recycle: refactor: SubsidyController 남편 인가 처리
    남편 인가 처리 및 지원금 상세 페이지 수정

--- src/main/java/foregg/foreggserver/controller/SubsidyController.java
+++ src/main/java/foregg/foreggserver/controller/SubsidyController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/subsidy")
+@PreAuthorize("isAuthenticated()")
 @Tag(name = "지원금 API")
 public class SubsidyController {
 
@@ -25,7 +26,6 @@ public class SubsidyController {
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
     })
-    @PreAuthorize("hasRole('ROLE_WIFE')")
     @PostMapping("")
     public ApiResponse<String> createSubsidy(@RequestBody SubsidyRequestDTO dto) {
         subsidyService.createSubsidy(dto);
@@ -37,7 +37,6 @@ public class SubsidyController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "SUBSIDY4001", description = "나의 지원금이 존재하지 않습니다"),
     })
-    @PreAuthorize("hasRole('ROLE_WIFE')")
     @PutMapping("/{id}")
     public ApiResponse<String> updateSubsidy(@PathVariable(name = "id") Long id, @RequestBody SubsidyRequestDTO dto) {
         subsidyService.updateSubsidy(id, dto);
@@ -49,24 +48,31 @@ public class SubsidyController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "SUBSIDY4001", description = "나의 지원금이 존재하지 않습니다"),
     })
-    @PreAuthorize("hasRole('ROLE_WIFE')")
     @DeleteMapping("/{id}")
     public ApiResponse<String> deleteSubsidy(@PathVariable(name = "id") Long id) {
         subsidyService.deleteSubsidy(id);
         return ApiResponse.onSuccess();
     }
 
+    @Operation(description = "지원금 상세 API")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "SUBSIDY4001", description = "나의 지원금이 존재하지 않습니다"),
+    })
+    @GetMapping("/{id}")
+    public ApiResponse<SubsidyRequestDTO> subsidyDetail(@PathVariable(name = "id") Long id) {
+        SubsidyRequestDTO result = subsidyQueryService.detailSubsidy(id);
+        return ApiResponse.onSuccess(result);
+    }
+
     @Operation(description = "회차별 지원금 API")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
     })
-    @PreAuthorize("hasRole('ROLE_WIFE')")
     @GetMapping("/byCount/{count}")
     public ApiResponse<SubsidyResponseDTO> getSubsidyByCount(@PathVariable(name = "count") int count) {
         SubsidyResponseDTO result = subsidyQueryService.getSubsidyByCount(count);
         return ApiResponse.onSuccess(result);
     }
-
-
 
 }
