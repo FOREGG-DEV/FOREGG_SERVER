b8a6f853f8ef86f3b9c00e504f54459ae233bdd8
kimdongjun <r2222911r@ajou.ac.kr>
2024-09-16 18:57:53 +0900

    :recycle: refactor: DailyResponseDTO 필드 변경
    DailyResponseDTO 필드 변경

--- src/main/java/foregg/foreggserver/dto/dailyDTO/DailyResponseDTO.java
+++ src/main/java/foregg/foreggserver/dto/dailyDTO/DailyResponseDTO.java
@@ -1,7 +1,6 @@
 package foregg.foreggserver.dto.dailyDTO;
 
 import foregg.foreggserver.domain.enums.DailyConditionType;
-import foregg.foreggserver.domain.enums.EmotionType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,10 +13,23 @@ import lombok.NoArgsConstructor;
 public class DailyResponseDTO {
 
     private Long id;
+    private int count;
+    private String date;
+    private String day;
     private DailyConditionType dailyConditionType;
     private String content;
-    private String date;
-    private EmotionType emotionType;
-    private String createAt;
+    private String imageUrl;
+    private String reply;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class DailyByCountResponseDTO {
+        private Long id;
+        private String date;
+        private DailyConditionType dailyConditionType;
+        private String content;
+    }
 
 }
