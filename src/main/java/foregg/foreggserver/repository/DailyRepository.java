17b61ef0985947ac75b9043ac9ecbd1272da0976
kimdongjun <r2222911r@ajou.ac.kr>
2024-09-16 18:57:11 +0900

    :recycle: refactor: DailyRepository 메서드 추가
    DailyRepository 메서드 추가

--- src/main/java/foregg/foreggserver/repository/DailyRepository.java
+++ src/main/java/foregg/foreggserver/repository/DailyRepository.java
@@ -2,7 +2,9 @@ package foregg.foreggserver.repository;
 
 import foregg.foreggserver.domain.Daily;
 import foregg.foreggserver.domain.User;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +16,9 @@ public interface DailyRepository extends JpaRepository<Daily, Long> {
     Optional<Daily> findByIdAndUser(Long id, User user);
 
     Optional<List<Daily>> findByUser(User user);
+
+    Optional<List<Daily>> findByUserAndCount(User user, int count);
+
+    @Query("SELECT d FROM Daily d WHERE d.date LIKE CONCAT(:date, '%') AND d.user = :user")
+    Daily findByDateAndUser(@Param("date") String date, @Param("user") User user);  // @Param 추가 및 CONCAT 사용
 }
