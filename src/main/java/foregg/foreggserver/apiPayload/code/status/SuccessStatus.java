b65b74dc564e21d99768cad3b2c95e053289ae53
kimdongjun <r2222911r@ajou.ac.kr>
2024-10-14 16:00:03 +0900

    :recycle: refactor: 리다이렉트 SuccessStatus 추가
    리다이렉트 SuccessStatus 추가

--- src/main/java/foregg/foreggserver/apiPayload/code/status/SuccessStatus.java
+++ src/main/java/foregg/foreggserver/apiPayload/code/status/SuccessStatus.java
@@ -12,8 +12,9 @@ import org.springframework.http.HttpStatus;
 public enum SuccessStatus implements BaseCode {
 
     // 일반적인 응답
-    _OK(HttpStatus.OK, "COMMON200", "성공입니다.");
+    _OK(HttpStatus.OK, "COMMON200", "성공입니다."),
     // 멤버 관련 응답
+    _MOVED_PERMANENTLY(HttpStatus.MOVED_PERMANENTLY, "REDIRECT301", "리다이렉트 필요");
 
 
     // ~~~ 관련 응답
