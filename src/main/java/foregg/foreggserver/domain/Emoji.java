e5edfc305b47db25188630590072e22d9c96d01d
kimdongjun <r2222911r@ajou.ac.kr>
2024-10-14 16:39:48 +0900

    :recycle: refactor: 이모지 테스트
    이모지 테스트

---
+++ src/main/java/foregg/foreggserver/domain/Emoji.java
@@ -0,0 +1,22 @@
+package foregg.foreggserver.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Emoji {
+
+    @Id
+    private String unicode;
+
+}
