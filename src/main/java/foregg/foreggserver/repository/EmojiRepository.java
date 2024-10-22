c378719180d7939ebc1b709bf2bd7177934e2ada
kimdongjun <r2222911r@ajou.ac.kr>
2024-10-22 11:49:02 +0900

    :wastebasket: refactor: 불필요 클래스 삭제
    불필요 클래스 삭제

--- src/main/java/foregg/foreggserver/repository/EmojiRepository.java
+++
@@ -1,8 +0,0 @@
-package foregg.foreggserver.repository;
-
-import foregg.foreggserver.domain.Emoji;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmojiRepository extends JpaRepository<Emoji, Long> {
-
-}
