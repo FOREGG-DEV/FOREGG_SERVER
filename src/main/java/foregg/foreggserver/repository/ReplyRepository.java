aa024c695ff528d0dbf6759cc8fa8dbaefbf2c2d
kimdongjun <r2222911r@ajou.ac.kr>
2024-11-21 16:20:14 +0900

    :recycle: refactor: ReplyRepository 메서드 추가
    ReplyRepository 메서드 추가

--- src/main/java/foregg/foreggserver/repository/ReplyRepository.java
+++ src/main/java/foregg/foreggserver/repository/ReplyRepository.java
@@ -1,8 +1,12 @@
 package foregg.foreggserver.repository;
 
 import foregg.foreggserver.domain.Reply;
+import foregg.foreggserver.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    List<Reply> findByReceiver(User user);
 
 }
