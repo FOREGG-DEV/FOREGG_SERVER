0eb425ed8fda59885611684627ca0da9aed13c49
kimdongjun <r2222911r@ajou.ac.kr>
2025-01-02 14:59:44 +0900

    :recycle: refactor: 엔티티 이름 수정
    Injection -> Medicine

--- src/main/java/foregg/foreggserver/domain/Medical.java
+++ src/main/java/foregg/foreggserver/domain/Medical.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Injection extends BaseEntity {
+public class Medical extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
