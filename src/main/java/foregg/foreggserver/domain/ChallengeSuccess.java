09167a786faa1371954b54a96a5fcb588c3bec8f
kimdongjun <r2222911r@ajou.ac.kr>
2025-01-15 00:17:25 +0900

    :recycle: refactor: ChallengeSuccess 엔티티 생성
    ChallengeSuccess 엔티티 생성

---
+++ src/main/java/foregg/foreggserver/domain/ChallengeSuccess.java
@@ -0,0 +1,30 @@
+package foregg.foreggserver.domain;
+
+import foregg.foreggserver.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ChallengeSuccess extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "challenge_participation_id")
+    private ChallengeParticipation challengeParticipation;
+
+    @Column(nullable = false)
+    private String date;
+
+    private String comment;
+
+}
